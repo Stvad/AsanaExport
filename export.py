6912295b343ed850263626df58be1411541772b6
Vladyslav Sitalo <root@stvad.org>
2015-10-30 19:38:54 +0200

    Json export implementation

--- export.py
+++ export.py
@@ -1,15 +1,14 @@
 import argparse
 import asana
-import os
 from getpass import getpass
-from pprint import pprint
 import json
+
+from pathlib import Path
 
 #compress name = asana-export-timestamp
 #directory to save
 #specific workspace
 #specific project
-import sys
 
 
 def authorize(token):
@@ -44,20 +43,60 @@ def print_data(client):
     # for task in tasks:
     #     print(task)
 
-def export_data(destination):
 
-    auth_token = getpass
-    client = authorize(auth_token)
+class AsanaExporter:
+    def __init__(self):
+        self.destination_path = None
+        self.client = None
 
-    os.makedirs()
+    def export_data(self, destination):
+
+        self.destination_path = Path(destination)
+
+        auth_token = getpass()
+        self.client = authorize(auth_token)
+
+        workspaces = list(self.client.workspaces.find_all(expand=["this"]))
+        for workspace in workspaces:
+            self.process_workspace(workspace)
+
+    def process_workspace(self, workspace):
+        workspace_path = self.destination_path.joinpath(workspace['name'])
+        workspace_path.mkdir(parents=True, exist_ok=True)
+
+        metadata_path = workspace_path.joinpath('workspace_information.json')
+        with metadata_path.open(mode='w', encoding='utf-8') as metadata_file:
+            metadata_file.write(self.get_nice_json(workspace))
+
+        projects = self.client.projects.find_by_workspace(workspace['id'], iterator_type=None, expand=["this"])
+        for project in projects:
+            self.process_project(workspace_path, project)
+
+    def process_project(self, base_path, project):
+        project_path = base_path.joinpath(project['name']+'.json')
+        tasks = list(self.client.projects.tasks(project['id'], expand=["this", "subtasks+"]))
+        project['tasks'] = tasks
+
+        with project_path.open(mode='w', encoding='utf-8') as project_file:
+            project_file.write(self.get_nice_json(project))
+
+
+    @classmethod
+    def get_nice_json(self, object): #move out from the class
+        return json.dumps(object, sort_keys=True, indent=2, ensure_ascii=False)#.encode('utf-8')
 
 
 if __name__ == '__main__':
-    token = getpass()
-    client = authorize(token)
-    print_data(client)
+    # token = getpass()
+    # client = authorize(token)
+    # print_data(client)
 
     parser = argparse.ArgumentParser(description='Export your data from Asana.')
     parser.add_argument('destination')
     args = parser.parse_args()
+
+    asana_exporter = AsanaExporter()
+    asana_exporter.export_data(args.destination)
+
+    print("Exported successfully to: "+args.destination)
 
